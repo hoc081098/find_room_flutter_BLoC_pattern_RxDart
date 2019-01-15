422f4f648b45e322f3dcafa41aec7a5263f21080
Petrus Hoc <hoc081098@gmail.com>
2019-01-15 12:48:05 +0700

    Update

---
+++ lib/models/district_entity.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:find_room/models/firebase_model.dart';
+import 'package:find_room/utitls/model_json_convert.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part 'province_entity.g.dart';
+
+@JsonSerializable()
+@immutable
+class ProvinceEntity implements FirebaseModel {
+  final String name;
+  final String documentID;
+
+  const ProvinceEntity({
+    this.name,
+    this.documentID,
+  });
+
+  factory ProvinceEntity.fromDocumentSnapshot(DocumentSnapshot doc) =>
+      _$ProvinceEntityFromJson(withId(doc));
+
+  Map<String, dynamic> toJson() => _$ProvinceEntityToJson(this);
+
+  @override
+  String get id => documentID;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProvinceEntity &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          documentID == other.documentID;
+
+  @override
+  int get hashCode => name.hashCode ^ documentID.hashCode;
+
+  @override
+  String toString() => 'ProvinceEntity{name: $name, documentID: $documentID}';
+}
---
+++ lib/models/ward_entity.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:find_room/models/firebase_model.dart';
+import 'package:find_room/utitls/model_json_convert.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+@JsonSerializable()
+@immutable
+class DistrictEntity implements FirebaseModel {
+  final String name;
+  final String documentID;
+
+  const DistrictEntity({
+    this.name,
+    this.documentID,
+  });
+
+  factory DistrictEntity.fromDocumentSnapshot(DocumentSnapshot doc) =>
+      _$DistrictEntityFromJson(withId(doc));
+
+  Map<String, dynamic> toJson() => _$DistrictEntityToJson(this);
+
+  @override
+  String get id => documentID;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DistrictEntity &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          documentID == other.documentID;
+
+  @override
+  int get hashCode => name.hashCode ^ documentID.hashCode;
+
+  @override
+  String toString() => 'DistrictEntity{name: $name, documentID: $documentID}';
+}
